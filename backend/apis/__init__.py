c3059f673cb1c0d4996af0eaad6ce7738fb5d8f6
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-03-06 17:03:20 -0300

    payment type - create payment type model - create bases classes models - create json parser - create API

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -1,8 +1,10 @@
 from flask_restful import Api
 
 from backend.apis.base_resource import BaseResource
+from backend.apis.payment_type.payment_type import PaymentTypeResource
 
 api = Api()
 
 # resources
 api.add_resource(BaseResource, '/base')
+api.add_resource(PaymentTypeResource, '/payment_type')
