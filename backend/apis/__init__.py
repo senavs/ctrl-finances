f422239349074fea296186fe3d4a2f07c2b806c7
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-03-11 13:13:59 -0300

    new database schema - create new database schema - delete old movement and payment_type model and API

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -1,12 +1,8 @@
 from flask_restful import Api
 
 from backend.apis.base_resource import BaseResource
-from backend.apis.payment_type.payment_type import PaymentTypeResource
-from backend.apis.movement.movement import MovementResource
 
 api = Api()
 
 # resources
 api.add_resource(BaseResource, '/base')
-api.add_resource(PaymentTypeResource, '/payment_type')
-api.add_resource(MovementResource, '/movement')
