6546b44461bd78b6d0cd9f71f726c21afc275cf2
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-03-11 21:28:34 -0300

    user - create user model - create user api (GET, POST, PUT, DELETE) - create user schema json - add "invalid password" constant message
    other schemas
    - fixed type (id to str)
    
    requirements
    - add werkzeug==0.16.1 to fix flask-bcrypt check_password method

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -1,10 +1,12 @@
 from flask_restful import Api
 
+from backend.apis.user import UserResource
 from backend.apis.movement_type import MovementTypeResource
 from backend.apis.movement_tag import MovementTagResource
 
 api = Api()
 
 # resources
+api.add_resource(UserResource, '/user')
 api.add_resource(MovementTypeResource, '/movement_type')
 api.add_resource(MovementTagResource, '/movement_tag')
